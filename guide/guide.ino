65ccb3ec336f1d1555490b06b1d9ee8498fb2dcf
Will Langas <wrlcubs@gmail.com>
2018-01-09 09:42:36 -0600

    Create guide.ino

---
+++ guide/guide.ino
@@ -0,0 +1,29 @@
+#include <FastLED.h>
+
+#define LED_PIN     1
+#define NUM_LEDS    15
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER RGB
+CRGB leds[NUM_LEDS];
+
+#define UPDATES_PER_SECOND 100
+
+void setup() {
+  delay(3000);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(BRIGHTNESS);
+
+  currentPalette = RainbowColors_p
+  currentBlending = LINEARBLEND;
+}
+
+void loop() {
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(750);
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(750);
+}
